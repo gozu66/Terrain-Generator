958fc497be3b3645f78825888c9ad637ea491351
gozu66 <gozu66@gmail.com>
2016-05-13 02:50:34 +0100

    Noise, Map Generator and display setup

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Noise {
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scaler)
+    {
+        if(scaler <= 0)
+        {
+            scaler = 0.0001f;
+        }
+
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for(int y = 0; y < mapHeight; y++)
+            {
+                float sampleX = x / scaler;
+                float sampleY = y / scaler;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
